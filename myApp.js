c3f8c8671a3d46a2b89b2b1b69ef5b81d636983a
mahmoud mheisen <mahmoudmheisen91@gmail.com>
2020-03-27 18:54:18 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,15 +1,15 @@
 /**********************************************
-* 4. Applied InfoSec Challenges
-* =============================
-***********************************************/
+ * 4. Applied InfoSec Challenges
+ * =============================
+ ***********************************************/
 
-var express = require('express'); // Do Not Edit
-var app = express();              // Do Not Edit
+var express = require("express"); // Do Not Edit
+var app = express(); // Do Not Edit
 
 // ----
 
 /** - Challenges - *
-********************/ 
+ ********************/
 
 /** 1) Install and require `helmet` */
 
@@ -17,7 +17,7 @@ var app = express();              // Do Not Edit
 // Express apps by setting various HTTP headers.
 // Install the package, then require it.
 
-
+let helmet = require("helmet");
 
 /** 2) Hide potentially dangerous information - `helmet.hidePoweredBy()` */
 
@@ -30,8 +30,7 @@ var app = express();              // Do Not Edit
 // people off. e.g. `helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' })`
 
 // Use `helmet.hidePoweredBy()``
-
-
+app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
 
@@ -45,8 +44,7 @@ var app = express();              // Do Not Edit
 
 // We don't need our app to be framed, so you should use `helmet.frameguard()`
 // passing to it the configuration object `{action: 'deny'}`
-
- 
+app.use(helmet.frameguard({ action: "deny" }));
 
 /** 4) Mitigate the risk of XSS - `helmet.xssFilter()` */
 
@@ -68,8 +66,7 @@ var app = express();              // Do Not Edit
 // It still has limited support.
 
 // Use `helmet.xssFilter()`
-
-
+app.use(helmet.xssFilter());
 
 /** 5) Avoid inferring the response MIME type - `helmet.noSniff()` */
 
@@ -81,8 +78,7 @@ var app = express();              // Do Not Edit
 // instructing the browser to not bypass the provided `Content-Type`.
 
 // Use `helmet.noSniff()`
-
-
+app.use(helmet.noSniff());
 
 /** 6) Prevent IE from opening *untrusted* HTML - `helmet.ieNoOpen()` */
 
@@ -94,12 +90,11 @@ var app = express();              // Do Not Edit
 // to prevent IE users from executing downloads in the *trusted* site's context.
 
 // Use `helmet.ieNoOpen()`
-
-
+app.use(helmet.ieNoOpen());
 
 /**  7) Ask browsers to access your site via HTTPS only - `helmet.hsts()` */
 
-// HTTP Strict Transport Security (HSTS) is a web security policy mechanism which 
+// HTTP Strict Transport Security (HSTS) is a web security policy mechanism which
 // helps to protect websites against protocol downgrade attacks and cookie hijacking.
 // If your website can be accessed via HTTPS you can ask user's browsers
 // to avoid using insecure HTTP. Setting the header `Strict-Transport-Security`
@@ -108,13 +103,18 @@ var app = express();              // Do Not Edit
 // the initial request.
 
 // Configure `helmet.hsts()` to instruct browsers to use HTTPS for the next
-// **90 days**, passing the config object {maxAge: timeInSeconds}. 
-// HyperDev already has **hsts** enabled, to override its settings you need to 
-// set the field `force` to `true` in the config object. To not alter hyperdev security 
+// **90 days**, passing the config object {maxAge: timeInSeconds}.
+// HyperDev already has **hsts** enabled, to override its settings you need to
+// set the field `force` to `true` in the config object. To not alter hyperdev security
 // policy we will intercept and restore the header, after inspecting it for testing.
 
-var ninetyDaysInSeconds = 90*24*60*60;
-
+var ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(
+  helmet.hsts({
+    maxAge: ninetyDaysInSeconds,
+    force: true
+  })
+);
 
 //**Note**:
 // Configuring HTTPS on a custom website requires the acquisition of a domain,
@@ -133,8 +133,6 @@ var ninetyDaysInSeconds = 90*24*60*60;
 
 // Use `helmet.dnsPrefetchControl()`
 
-
-
 /** 9) Disable Client-Side Caching - `helmet.noCache()` */
 
 // If you are releasing an update for your website, and you want your users
@@ -144,8 +142,6 @@ var ninetyDaysInSeconds = 90*24*60*60;
 // use this option only when there is a real need.
 
 // Use helmet.noCache()
-
-
 
 /** 10) Content Security Policy - `helmet.contentSecurityPolicy()` */
 
@@ -171,20 +167,18 @@ var ninetyDaysInSeconds = 90*24*60*60;
 // Set also the `scriptSrc` directive so that you will allow scripts to be downloaded
 // from your website, and from the domain `trusted-cdn.com`.
 //
-// **Hint**: 
-// in the `"'self'"` keyword, the single quotes are part of the keyword itself, 
+// **Hint**:
+// in the `"'self'"` keyword, the single quotes are part of the keyword itself,
 // so it needs to be enclosed in **double quotes** to be working.
 
-
-
-/** TIP: */ 
+/** TIP: */
 
 // `app.use(helmet())` will automatically include all the middleware
 // presented above, except `noCache()`, and `contentSecurityPolicy()`,
-// but these can be enabled if necessary. You can also disable or 
+// but these can be enabled if necessary. You can also disable or
 // set any other middleware individually, using a configuration object.
 
-// // - Example - 
+// // - Example -
 // app.use(helmet({
 //   frameguard: {              // configure
 //     action: 'deny'
@@ -205,13 +199,13 @@ var ninetyDaysInSeconds = 90*24*60*60;
 // ---- DO NOT EDIT BELOW THIS LINE ---------------------------------------
 
 module.exports = app;
-var api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+var api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT || 3000, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
